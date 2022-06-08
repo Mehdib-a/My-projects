1a95889e0fc3ff7b38024c8ae84994143e160ca6
MohammadMehdi Bagheri <107081455+Mehdib-a@users.noreply.github.com>
2022-06-08 12:15:01 +0430

    Create Addition-and-subtraction-in-solidity.sol

---
+++ Addition-and-subtraction-in-solidity.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+ contract functionadd{
+    function sum(uint x , uint y) external pure returns (uint){
+       return x+y;
+    }
+    function minus(uint x , uint y) external pure returns (uint){
+        return x-y;
+    }
+ }
