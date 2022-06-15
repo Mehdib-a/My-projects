33d3c6757fb357bdaf79ea620b68774f2fdaf7ca
MohammadMehdi Bagheri <107081455+Mehdib-a@users.noreply.github.com>
2022-06-15 12:23:03 +0430

    Create MyTokenErc20.sol

---
+++ MyTokenErc20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract MyERC20token is ERC20 {
+    constructor (uint256 myTotalsupply) ERC20("MyTokenErc20","MTE") {
+        _mint(msg.sender , myTotalsupply);
+    }
+}
